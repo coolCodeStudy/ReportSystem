371aef3c0d846a137af5f0d8fb99b5f941aa2e55
coolCodeStudy <spicyorangee@gmail.com>
2026-02-16 16:23:42 +0800

    Upgrade to Java 17 and increase SSH timeout

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
@@ -31,6 +31,6 @@ tasks.withType<Test> {
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
